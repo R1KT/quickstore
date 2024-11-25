1dc6d26dd36eafa1e8f672fbf9d864a99ee2a322
Abhineeth <abhistechs2000@gmail.com>
2024-11-25 22:45:40 +0530

    Initial Commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login_view
+from .views import login_view, dashboard_view
 
 urlpatterns = [
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
