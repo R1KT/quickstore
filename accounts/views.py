dd12d3eb64a662a5f0a136a7625723ca1e8c7c11
Abhineeth <abhistechs2000@gmail.com>
2024-11-25 22:03:01 +0530

    Initial Commit

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username, password)
 
         if user:
