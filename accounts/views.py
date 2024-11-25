f6f5c54da291babff4ba9297ddc0d484339f0c33
Abhineeth <abhistechs2000@gmail.com>
2024-11-25 22:05:27 +0530

    Initial Commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.http import HttpResponse
 
 def login_view(request):
     if request.method == 'POST':
