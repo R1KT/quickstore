a49184d8b321fadc356f99562f2341135e792f6c
Abhineeth <abhistechs2000@gmail.com>
2024-11-25 22:12:32 +0530

    Initial Commit

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username, password)
+        user = authenticate(request, username=username, password=password)
 
         if user:
             login(request, user)
@@ -16,5 +16,5 @@ def login_view(request):
         else:
             messages.error(request, "Login Failed!!")
 
-        return render(request, 'login/login.html')
+    return render(request, 'login/login.html')
 
