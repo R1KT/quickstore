8804c673ad2c4dded06146fd23cfdc4626b487ca
Abhineeth <abhistechs2000@gmail.com>
2024-11-26 22:52:00 +0530

    Initial Commit

--- quickstore/settings.py
+++ quickstore/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-    # os.path.join(BASE_DIR, 'accoounts/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles'),
+#     # os.path.join(BASE_DIR, 'accoounts/static'),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
