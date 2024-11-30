bd3fe9f1643a4e92c3cd7da0dd77ed358bd5e62a
Abhineeth <abhistechs2000@gmail.com>
2024-11-30 21:48:44 +0530

    Initial Commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required
+# @login_required
 def homeView(request):
     request.session.set_expiry(0)
     return render(request, 'home/home.html')
