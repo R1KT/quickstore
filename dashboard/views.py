6c1943b45f0de7001ee91051ef0bb00e61870712
Abhineeth <abhistechs2000@gmail.com>
2024-11-30 22:58:23 +0530

    Initial Commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def homeView(request):
     if request.method == 'GET':
-        return render(request, 'home/home.html')
+        return render(request, 'home\home.html')
 
 
 
