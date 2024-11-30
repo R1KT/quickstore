5798be0c90571265ed1e7c69843ea1366fb7542a
Abhineeth <abhistechs2000@gmail.com>
2024-11-30 23:00:57 +0530

    Initial Commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,21 +6,17 @@ from django.contrib.auth.decorators import login_required
 
 def loginView(request):
     if request.method == 'POST':
-        print("Form submitted")
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
 
         if user:
-            print("User authenticated")
             login(request, user)
             if 'next' in request.POST:
-                print(f"Redirecting to: next_url")
                 return redirect(request.POST['next'])
             else:
                 return redirect('dashboard:home')
         else:
-            print("Invalid credentials")
             return render(request, "login\login.html", {'error' : 'Invalid Credentials'})
 
     return render(request, 'login\login.html')
