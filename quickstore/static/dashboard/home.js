6dfd1a6ddb161da549e4489fcd865958a90bfa71
Abhineeth <abhistechs2000@gmail.com>
2024-12-02 22:43:03 +0530

    Initial Commit

--- quickstore/static/dashboard/home.js
+++ quickstore/static/dashboard/home.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Define paths to external HTML files for each section
     const contentMap = {
-        residentContent: 'screens/residents.html',
+        residentContent: '/dashboard/screens/residents.html',
     };
 
     // Add click event listeners to each nav link
