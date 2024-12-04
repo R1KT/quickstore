c6857125fbe8553dbbd7070e63d7df5d1fac81b7
Abhineeth <abhistechs2000@gmail.com>
2024-12-04 22:19:35 +0530

    Initial Commit

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Register(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    block = models.CharField(max_length=5)
+    apartment = models.IntegerField()
+
+    def __str__(self):
+        building = f"{self.block}{self.apartment}"
+        return building
