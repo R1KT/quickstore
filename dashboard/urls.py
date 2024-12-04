c7bea9102d248f0799116f40f8a9f52e113a6ff4
Abhineeth <abhistechs2000@gmail.com>
2024-12-04 19:23:58 +0530

    Initial Commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,7 @@ app_name = "dashboard"
 urlpatterns = [
     path('home/', homeView, name='home'),
     path('residents/', residentsView, name='residents'),
-    path('residents/new/', residentsView, name='residents'),
+    path('residents/new/', newResidentView, name='newResident'),
     path('services/', servicesView, name='services'),
     path('guests/', guestsView, name='guests'),
 ]
